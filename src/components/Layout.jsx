cef77a6a7fb069428854c8ddbf9808d6f4348865
parkjihye12 <qkr-wlg@hanmail.net>
2024-05-16 11:53:55 +0900

    first commit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+
+const Layout = () => {
+  return (
+    <div className="bg-gray-400 max-w-screen-md mx-auto min-h-screen shadow-md">
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
